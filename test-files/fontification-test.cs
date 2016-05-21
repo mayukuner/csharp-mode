004779264e997fea0259ad86171eb884357433e9
Jostein Kjønigsen <jostein@kjonigsen.net>
2016-05-21 22:17:48 +0200

    New test-macros for assessments.

--- test-files/fontification-test.cs
+++ test-files/fontification-test.cs
@@ -1,6 +1,6 @@
-public const string Literal1 = @"literal without trailing slash";
-public const bool Reference = true;
-public const string Literal2 = @"literal with trailing slash\"; 
-public const bool Reference = true;
+public const string Literal1 = @"literal "" without \\ trailing slash";
+public const bool1 Reference1 = false;
+public const string Literal2 = @"literal "" with \\ trailing slash\"; 
+public const bool2 Reference2 = true;
 public const string Literal3 = @"multi-line
 literal";
